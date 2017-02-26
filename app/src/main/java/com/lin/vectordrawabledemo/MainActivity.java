24bf66899453a7f4ce36242fb565f929f526c5b2
“shenglintang” <793058640@qq.com>
2017-02-26 10:21:22 +0800

    add new file

---
+++ app/src/main/java/com/lin/vectordrawabledemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.lin.vectordrawabledemo;
+
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*
+    * VectorDrawable实现路径变换动画的兼容性问题，
+    * 故路径变换动画目前存在兼容性问题。
+    * 不能在4.X版本运行，这一点格外注意。
+    */
+    public void onClick(View view) {
+        ImageView imageView = (ImageView) view;
+        AnimatedVectorDrawable vectorDrawable = (AnimatedVectorDrawable) getDrawable(R.drawable.fivestar_anim);
+        imageView.setImageDrawable(vectorDrawable);
+        if (vectorDrawable != null) vectorDrawable.start();
+
+//        Drawable drawable = imageView.getDrawable();
+
+//        if (drawable instanceof Animatable) {
+//            ((Animatable) drawable).start();
+//        }
+    }
+}
